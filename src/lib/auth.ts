46ce4e082df28ff0661b4b756cf78efdf81f98a1
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-01 00:22:59 +0330

    Working on db

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "./db"
+import GoogleProvider from "next-auth/providers/google"
+
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+      // ...add more providers here
+    ],
+  
